8fdd15fc8e5e5f8996e59391e32a90a2eeb01509
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2014-10-18 20:05:06 +0300

    Bugfix: ArrowUp / ArrowDown in editor now works properly (doesn't commit changes)

--- qt-app/src/panel/filelistwidget/delegate/cfilelistitemdelegate.cpp
+++ qt-app/src/panel/filelistwidget/delegate/cfilelistitemdelegate.cpp
@@ -31,59 +31,65 @@ bool CFileListItemDelegate::eventFilter(QObject * object, QEvent * event)
 {
 	QWidget *editor = qobject_cast<QWidget*>(object);
 	if (!editor)
-	   return false;
+		return false;
 
 	if (event->type() == QEvent::KeyPress)
 	{
-	   switch (static_cast<QKeyEvent *>(event)->key())
-	   {
-	   case Qt::Key_Tab:
+		switch (static_cast<QKeyEvent *>(event)->key())
+		{
+		case Qt::Key_Tab:
 			emit closeEditor(editor, EditNextItem);
 			return true;
-	   case Qt::Key_Backtab:
+		case Qt::Key_Backtab:
 			emit closeEditor(editor, EditPreviousItem);
 			return true;
-	   case Qt::Key_Enter:
-	   case Qt::Key_Return:
-		   if (qobject_cast<QTextEdit *>(editor) || qobject_cast<QPlainTextEdit *>(editor))
-			   return false; // don't filter enter key events for QTextEdit
+		case Qt::Key_Enter:
+		case Qt::Key_Return:
+			if (qobject_cast<QTextEdit *>(editor) || qobject_cast<QPlainTextEdit *>(editor))
+				return false; // don't filter enter key events for QTextEdit
 
-		   // commit data
-		   emit commitData(editor);
-		   emit closeEditor(editor);
-		   return true;
-	   case Qt::Key_Escape:
-		   // don't commit data
-		   emit closeEditor(editor, QAbstractItemDelegate::RevertModelCache);
-		   return true;
-	   default:
-		   return false;
-	   }
+			// commit data
+			emit commitData(editor);
+			emit closeEditor(editor);
+			return true;
+		case Qt::Key_Escape:
+			// don't commit data
+			emit closeEditor(editor, QAbstractItemDelegate::RevertModelCache);
+			return true;
+		case Qt::Key_Up:
+		case Qt::Key_Down:
+			// don't commit data
+			emit closeEditor(editor, QAbstractItemDelegate::RevertModelCache);
+			// Return false so that the TreeView will handle this event and highlight the next item properly
+			return false;
+		default:
+			return false;
+		}
 
 	}
 	else if (event->type() == QEvent::FocusOut || (event->type() == QEvent::Hide && editor->isWindow()))
 	{
-	   //the Hide event will take care of he editors that are in fact complete dialogs
-	   if (!editor->isActiveWindow() || (QApplication::focusWidget() != editor))
-	   {
-		   QWidget *w = QApplication::focusWidget();
-		   while (w)
-		   { // don't worry about focus changes internally in the editor
-			   if (w == editor)
-				   return false;
-			   w = w->parentWidget();
+		//the Hide event will take care of he editors that are in fact complete dialogs
+		if (!editor->isActiveWindow() || (QApplication::focusWidget() != editor))
+		{
+			QWidget *w = QApplication::focusWidget();
+			while (w)
+			{ // don't worry about focus changes internally in the editor
+				if (w == editor)
+					return false;
+				w = w->parentWidget();
 			}
 
-		   emit closeEditor(editor, NoHint);
-	   }
+			emit closeEditor(editor, NoHint);
+		}
 	}
 	else if (event->type() == QEvent::ShortcutOverride)
 	{
-	   if (static_cast<QKeyEvent*>(event)->key() == Qt::Key_Escape)
-	   {
-		   event->accept();
-		   return true;
-	   }
+		if (static_cast<QKeyEvent*>(event)->key() == Qt::Key_Escape)
+		{
+			event->accept();
+			return true;
+		}
 	}
 	return false;
 }
