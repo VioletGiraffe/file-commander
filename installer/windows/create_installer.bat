81131b1011069f8a01ccca43cbc4c6e1a3e02f53
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2015-03-17 17:33:57 +0200

    installer creation script turned into a complete one-click-build script

--- installer/windows/create_installer.bat
+++ installer/windows/create_installer.bat
@@ -1,7 +1,14 @@
 rem set QTDIR=c:\Development\Qt\4.8.5\
-set QTDIR=k:\Qt\5\5.4\msvc2013_opengl\
+set QTDIR=c:\Development\Qt\Qt5\5.4\msvc2013_opengl\
 
 RMDIR  /S /Q binaries\
+
+pushd ..\..\
+%QTDIR%\bin\qmake.exe -tp vc -r
+popd
+
+call "%VS120COMNTOOLS%VsDevCmd.bat"
+msbuild ..\..\file-commander.sln /t:Build /p:Configuration=Release;PlatformToolset=v120_xp
 
 xcopy /R /Y ..\..\bin\FileCommander.exe binaries\
 xcopy /R /Y ..\..\bin\plugin_*.dll binaries\
@@ -10,9 +17,7 @@ xcopy /R /Y %QTDIR%\bin\Qt5Core.dll binaries\Qt\
 xcopy /R /Y %QTDIR%\bin\Qt5Gui.dll binaries\Qt\
 xcopy /R /Y %QTDIR%\bin\Qt5Widgets.dll binaries\Qt\
 
-xcopy /R /Y %QTDIR%\bin\icudt52.dll binaries\Qt\
-xcopy /R /Y %QTDIR%\bin\icuin52.dll binaries\Qt\
-xcopy /R /Y %QTDIR%\bin\icuuc52.dll binaries\Qt\
+xcopy /R /Y %QTDIR%\bin\icu*.dll binaries\Qt\
 
 xcopy /R /Y %QTDIR%\plugins\imageformats\qgif.dll binaries\Qt\imageformats\
 xcopy /R /Y %QTDIR%\plugins\imageformats\qico.dll binaries\Qt\imageformats\
@@ -23,6 +28,5 @@ xcopy /R /Y %QTDIR%\plugins\platforms\qwindows.dll binaries\Qt\platforms\
 
 xcopy /R /Y %SystemRoot%\SysWOW64\msvcr120.dll binaries\msvcr\
 xcopy /R /Y %SystemRoot%\SysWOW64\msvcp120.dll binaries\msvcr\
-xcopy /R /Y %SystemRoot%\SysWOW64\vcomp120.dll binaries\msvcr\
 
 "c:\Program Files (x86)\Inno Setup 5\compil32" /cc setup.iss
