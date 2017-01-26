fed07391abdc24f77551f04fc5d593dd86bd17a0
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2017-01-26 11:25:15 +0200

    CFileCommanderToolPlugin class expanded

--- file-commander-core/src/plugininterface/cfilecommandertoolplugin.h
+++ file-commander-core/src/plugininterface/cfilecommandertoolplugin.h
@@ -2,8 +2,38 @@
 
 #include "cfilecommanderplugin.h"
 
+DISABLE_COMPILER_WARNINGS
+#include <QIcon>
+#include <QString>
+RESTORE_COMPILER_WARNINGS
+
+#include <stdint.h>
+#include <vector>
+
 class CFileCommanderToolPlugin : public CFileCommanderPlugin
 {
 public:
+
+	// A command is an action exported by the plugin for integration into the main File Commander UI
+	class Command {
+	public:
+		Command(const QString& name, const QIcon& icon);
+
+		QString name() const;
+		const QIcon& icon() const;
+
+		QString id() const;
+
+	private:
+		const QString _displayName; // Translated, if appropriate
+		const QIcon _icon;
+		const uint32_t _id; // The unique ID for this command
+	};
+
 	PluginType type() override;
+
+	const std::vector<Command>& commands() const;
+
+private:
+	std::vector<Command> _commands;
 };
