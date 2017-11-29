39755fb78c216fa93b72904cfbcf17f1b20ffb07
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2017-11-29 14:56:26 +0300

    macOS build fixed

--- file-commander-core/src/diskenumerator/volumeinfohelper.hpp
+++ file-commander-core/src/diskenumerator/volumeinfohelper.hpp
@@ -11,11 +11,11 @@ RESTORE_COMPILER_WARNINGS
 #include <sys/statvfs.h>
 #include <errno.h>
 
-inline struct statvfs64 volumeInfoForPath(const QString& path)
+inline struct statvfs volumeInfoForPath(const QString& path)
 {
-	struct statvfs64 info;
+	struct statvfs info;
 	memset(&info, 0, sizeof(info));
-	if (statvfs64(path.toLocal8Bit().data(),&info) != 0)
+	if (statvfs(path.toLocal8Bit().data(),&info) != 0)
 		qInfo() << "Error occurred while calling statvfs64:\n" << strerror(errno);
 
 	return info;
