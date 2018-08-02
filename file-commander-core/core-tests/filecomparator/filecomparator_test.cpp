8a33725ca1138071a4c54c34f516cb0114891afb
VioletGiraffe <violet.giraffe@noreply.github.com>
2018-08-02 13:08:42 +0300

    CFileComparator test fixed and added to CI

--- file-commander-core/core-tests/filecomparator/filecomparator_test.cpp
+++ file-commander-core/core-tests/filecomparator/filecomparator_test.cpp
@@ -1,24 +1,40 @@
 #include "filecomparator/cfilecomparator.h"
 #include "crandomdatagenerator.h"
+#include "system/ctimeelapsed.h"
+#include "compiler/compiler_warnings_control.h"
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "../catch2/catch.hpp"
 
 #include "catch2_utils.hpp"
 
-#include <QDir>
+DISABLE_COMPILER_WARNINGS
 #include <QTemporaryDir>
+RESTORE_COMPILER_WARNINGS
+
+#include <iostream>
+
+uint32_t g_randomSeed = 0; // std::random seed
 
 TEST_CASE("CFileComparator identical files tests", "[CFileComparator]")
 {
-	QTemporaryDir sourceDirectory(QDir::tempPath() + "/" + CURRENT_TEST_NAME.c_str() + "_XXXXXX");
-	CRandomDataGenerator gen;
-	QFile fileA(sourceDirectory.filePath("A")), fileB(sourceDirectory.filePath("B"));
-	for (int i = 0; i < 1000; ++i)
+	QTemporaryDir sourceDirectory;
+	if (!sourceDirectory.isValid())
 	{
-		const int length = gen.randomInt(1000, 10 * 1024 * 1024);
+		FAIL();
+		return;
+	}
+
+	CRandomDataGenerator gen;
+	gen.setSeed(g_randomSeed);
+	QFile fileA(sourceDirectory.filePath("A")), fileB(sourceDirectory.filePath("B"));
+	CTimeElapsed timer(true);
+	timer.pause();
+	for (int i = 0; i < 500; ++i)
+	{
+		const int length = gen.randomInt(10, 3 * 1024 * 1024);
 		const auto data = gen.randomString(length).toLatin1();
-		if (!fileA.open(QFile::ReadWrite) || !fileB.open(QFile::ReadWrite))
+		if (!fileA.open(QFile::WriteOnly) || !fileB.open(QFile::WriteOnly))
 		{
 			FAIL();
 			return;
@@ -40,50 +56,124 @@ TEST_CASE("CFileComparator identical files tests", "[CFileComparator]")
 		}
 
 		CFileComparator comparator;
+		timer.resume();
 		comparator.compareFiles(fileA, fileB, [](int) {}, [](CFileComparator::ComparisonResult result) {
 			CHECK(result == CFileComparator::Equal);
 		});
+		timer.pause();
 
 		fileA.close();
 		fileB.close();
 	}
+
+	std::cout << "Total time taken to process 1000 randomly sized files: " << timer.elapsed() / 1000.0f;
 }
 
 TEST_CASE("CFileComparator differing files tests", "[CFileComparator]")
 {
+
 	QTemporaryDir sourceDirectory(QDir::tempPath() + "/" + CURRENT_TEST_NAME.c_str() + "_XXXXXX");
 	CRandomDataGenerator gen;
+	gen.setSeed(g_randomSeed);
 	QFile fileA(sourceDirectory.filePath("A")), fileB(sourceDirectory.filePath("B"));
-	for (int i = 0; i < 1000; ++i)
+
+	SECTION("Completely random data")
 	{
-		const int length = gen.randomInt(1000, 10 * 1024 * 1024);
-		if (!fileA.open(QFile::ReadWrite) || !fileB.open(QFile::ReadWrite))
+		for (int i = 0; i < 500; ++i)
 		{
-			FAIL();
-			return;
+			const int length = gen.randomInt(10, 3 * 1024 * 1024);
+			if (!fileA.open(QFile::ReadWrite) || !fileB.open(QFile::ReadWrite))
+			{
+				FAIL();
+				return;
+			}
+
+			const auto dataA = gen.randomString(length).toLatin1();
+			const auto dataB = gen.randomString(length).toLatin1();
+			if (fileA.write(dataA) != length || fileB.write(dataB) != length)
+			{
+				FAIL();
+				return;
+			}
+
+			fileA.close();
+			fileB.close();
+
+			if (!fileA.open(QFile::ReadOnly) || !fileB.open(QFile::ReadOnly))
+			{
+				FAIL();
+				return;
+			}
+
+			CFileComparator comparator;
+			comparator.compareFiles(fileA, fileB, [](int) {}, [](CFileComparator::ComparisonResult result) {
+				CHECK(result == CFileComparator::NotEqual);
+			});
+
+			fileA.close();
+			fileB.close();
 		}
-
-		if (fileA.write(gen.randomString(length).toLatin1()) != length || fileB.write(gen.randomString(length).toLatin1()) != length)
-		{
-			FAIL();
-			return;
-		}
-
-		fileA.close();
-		fileB.close();
-
-		if (!fileA.open(QFile::ReadOnly) || !fileB.open(QFile::ReadOnly))
-		{
-			FAIL();
-			return;
-		}
-
-		CFileComparator comparator;
-		comparator.compareFiles(fileA, fileB, [](int) {}, [](CFileComparator::ComparisonResult result) {
-			CHECK(result == CFileComparator::NotEqual);
-		});
-
-		fileA.close();
-		fileB.close();
 	}
+
+	SECTION("Completely data differing in just one byte")
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			const int length = gen.randomInt(10, 3 * 1024 * 1024);
+			if (!fileA.open(QFile::ReadWrite) || !fileB.open(QFile::ReadWrite))
+			{
+				FAIL();
+				return;
+			}
+
+			const auto dataA = gen.randomString(length).toLatin1();
+			auto dataB = dataA;
+			dataB[dataB.size() - 1] = ~dataB[dataB.size() - 1];
+			if (fileA.write(dataA) != length || fileB.write(dataB) != length)
+			{
+				FAIL();
+				return;
+			}
+
+			fileA.close();
+			fileB.close();
+
+			if (!fileA.open(QFile::ReadOnly) || !fileB.open(QFile::ReadOnly))
+			{
+				FAIL();
+				return;
+			}
+
+			CFileComparator comparator;
+			comparator.compareFiles(fileA, fileB, [](int) {}, [](CFileComparator::ComparisonResult result) {
+				CHECK(result == CFileComparator::NotEqual);
+			});
+
+			fileA.close();
+			fileB.close();
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	Catch::Session session; // There must be exactly one instance
+
+							// Build a new parser on top of Catch's
+	using namespace Catch::clara;
+	auto cli
+		= session.cli() // Get Catch's composite command line parser
+		| Opt(g_randomSeed, "std::random seed") // bind variable to a new option, with a hint string
+		["--std-seed"]        // the option names it will respond to
+	("std::random seed"); // description string for the help output
+
+						  // Now pass the new composite back to Catch so it uses that
+	session.cli(cli);
+
+	// Let Catch (using Clara) parse the command line
+	const int returnCode = session.applyCommandLine(argc, argv);
+	if (returnCode != 0) // Indicates a command line error
+		return returnCode;
+
+	return session.run();
 }
