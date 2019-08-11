93e884c84f3a8926c5961e965c1bf568e001e8b2
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-08-11 12:53:35 +0300

    update_repository.bat improved

--- update_repository.bat
+++ update_repository.bat
@@ -1,9 +1,11 @@
-REM Update the main repo
-git pull
-
 REM Init the subrepos
 git submodule update --init --recursive
 git submodule foreach --recursive "git checkout master"
+
+REM Update the main repo
+git checkout master
+git fetch
+git pull
 
 REM Update the subrepos
 git submodule foreach --recursive "git pull"
