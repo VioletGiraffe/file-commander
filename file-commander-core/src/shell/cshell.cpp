175e239def5f6b290ff841c481be6e656f9276c5
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-02-03 14:25:19 +0200

    CShell bug fixed

--- file-commander-core/src/shell/cshell.cpp
+++ file-commander-core/src/shell/cshell.cpp
@@ -55,7 +55,13 @@ void OsShell::executeShellCommand(const QString& command, const QString& working
 {
 	std::thread([command, workingDir](){
 	#ifdef _WIN32
-		_wsystem((QString("pushd ") + workingDir + " && " + command).toStdWString().data());
+		WCHAR commandString[32768] = { 0 };
+		const auto len = (QStringLiteral("pushd ") + workingDir + " && " + command).toWCharArray(commandString);
+		//const auto len = QString{"cmd /c \"%1\""}.arg(command).toWCharArray(commandString);
+		assert_and_return_r(len < std::size(commandString), );
+		::_wsystem(commandString);
+		auto e = errno;
+		e = e;
 	#else
 		const int result = std::system((QString("cd ") + workingDir + " && " + command).toUtf8().data());
 		assert_r(result == 0);
