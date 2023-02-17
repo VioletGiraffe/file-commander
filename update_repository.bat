177cb11ab4d6f916ae27893cec92a0a497223e3d
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2023-02-17 23:03:12 +0200

    Subrepo

--- update_repository.bat
+++ update_repository.bat
@@ -1,11 +1,11 @@
-REM Init the subrepos
-git submodule update --init --recursive
-git submodule foreach --recursive "git checkout master"
-
 REM Update the main repo
 git checkout master
 git fetch
 git pull
+
+REM Init the subrepos
+git submodule update --init --recursive
+git submodule foreach --recursive "git checkout master"
 
 REM Update the subrepos
 git submodule foreach --recursive "git pull"
