a46e5c8c7e63e27539c2fcea286e38db6a6f13e0
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2023-04-09 15:27:40 +0300

    CFileSystemWatcherWindows::setPathToWatch bug fixed

--- file-commander-core/src/filesystemwatcher/cfilesystemwatcherwindows.cpp
+++ file-commander-core/src/filesystemwatcher/cfilesystemwatcherwindows.cpp
@@ -23,7 +23,15 @@ bool CFileSystemWatcherWindows::setPathToWatch(const QString& path) noexcept
 		return true;
 
 	WCHAR wPath[32768];
-	path.toWCharArray(wPath);
+	const auto pathLen = path.toWCharArray(wPath);
+	if (pathLen <= 0)
+		return false;
+
+	assert_debug_only(!path.contains('\\'));
+	wPath[pathLen] = 0;
+	if (path.endsWith('/'))
+		wPath[pathLen - 1] = 0; // FindFirstChangeNotificationW fails if the trailing slash is present!
+
 	_handle = ::FindFirstChangeNotificationW(wPath, false, FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_SIZE);
 	if (_handle == INVALID_HANDLE_VALUE)
 	{
