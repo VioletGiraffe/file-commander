1ef0faf82959b1dfba443f133d36a3a0fe414870
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2023-05-09 00:07:59 +0300

    update_repository.sh fixed

--- update_repository.sh
+++ update_repository.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Init the subrepos
-git submodule update --init --recursive
-git submodule foreach --recursive "git checkout master"
-
 # Update the main repo
 git checkout master
 git fetch
 git pull
+
+# Init the subrepos
+git submodule update --init --recursive
+git submodule foreach --recursive "git checkout master"
 
 # Update the subrepos
 git submodule foreach --recursive "git pull"
