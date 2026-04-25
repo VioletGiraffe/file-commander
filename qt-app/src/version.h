8a12d51d6d809f82d51d6fc6b3425b424c90a56e
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-04-25 12:21:47 +0300

    0.9.9.7

--- qt-app/src/version.h
+++ qt-app/src/version.h
@@ -6,5 +6,5 @@ DISABLE_COMPILER_WARNINGS
 #include <QString>
 RESTORE_COMPILER_WARNINGS
 
-#define VERSION_STRING QLatin1String("0.9.9.6")
+#define VERSION_STRING QLatin1String("0.9.9.7")
 #define REPO_NAME QLatin1String("VioletGiraffe/file-commander")
