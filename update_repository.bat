95dad76a139e1a63ac1543019f43ea6bb6a69402
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-07-11 20:09:46 +0300

    update_repository.bat/sh fixed

--- update_repository.bat
+++ update_repository.bat
@@ -5,7 +5,7 @@ git pull
 
 REM Init the subrepos
 git submodule update --init --recursive
-git submodule foreach --recursive "git checkout master"
+git submodule foreach --recursive "git remote set-head origin --auto >/dev/null 2>&1; b=$(git symbolic-ref --short refs/remotes/origin/HEAD 2>/dev/null); case $b in origin/*) git checkout ${b#origin/} ;; *) echo no default branch in $displaypath ;; esac"
 
 REM Update the subrepos
 git submodule foreach --recursive "git pull"
--- update_repository.sh
+++ update_repository.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Update the main repo
 git checkout master
@@ -7,7 +7,7 @@ git pull
 
 # Init the subrepos
 git submodule update --init --recursive
-git submodule foreach --recursive "git checkout master"
+git submodule foreach --recursive 'git remote set-head origin --auto >/dev/null 2>&1; b=$(git symbolic-ref --short refs/remotes/origin/HEAD 2>/dev/null); case $b in origin/*) git checkout ${b#origin/} ;; *) echo no default branch in $displaypath ;; esac'
 
 # Update the subrepos
 git submodule foreach --recursive "git pull"
