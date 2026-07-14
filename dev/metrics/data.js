435ba3ddfd6a59f4d0a4390475e9a46de89b440a
github-action-benchmark <github@users.noreply.github.com>
2026-07-14 20:04:48 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 8648f32b4c98a85d4caddb3d6f1faa1d1d1eea39

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784058273083,
+  "lastUpdate": 1784059488660,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -3976,6 +3976,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .text",
             "value": 631.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 369,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 22,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 36,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "8648f32b4c98a85d4caddb3d6f1faa1d1d1eea39",
+          "message": "Multiple cancellation issues fixed. Cancellation is \"release from pause\", a class of bugs eliminated when one more item or chunk could be copied before actually canceling",
+          "timestamp": "2026-07-14T22:59:39+03:00",
+          "tree_id": "e794f88b32dde7447216ee1aa6ebba42dcefa634",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/8648f32b4c98a85d4caddb3d6f1faa1d1d1eea39"
+        },
+        "date": 1784059485907,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 15802,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 8509,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1222.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 632,
             "unit": "KB"
           },
           {
