29ad20e78128c7ca9e2a43d99499a47f66a33578
github-action-benchmark <github@users.noreply.github.com>
2026-07-22 17:25:55 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 3c83d68871cb14b83380c42ca0b6a5cd2f81da5a

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784740457165,
+  "lastUpdate": 1784741154924,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -6026,6 +6026,75 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/VioletGiraffe/file-commander/commit/7ec40516c490628ca675daa68948b754e96f227c"
         },
         "date": 1784740454916,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 24579,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 10797,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1245,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 649.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 372.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 21,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 37,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "3c83d68871cb14b83380c42ca0b6a5cd2f81da5a",
+          "message": "CI: always run tests on manual dispatch",
+          "timestamp": "2026-07-22T20:20:09+03:00",
+          "tree_id": "1d7856cae8887205931d158122e4f6959b9123c5",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/3c83d68871cb14b83380c42ca0b6a5cd2f81da5a"
+        },
+        "date": 1784741151417,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
