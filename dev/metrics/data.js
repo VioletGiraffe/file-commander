5b9ed8b15f89ea544ebf4d6b49a7251b840ec3a7
github-action-benchmark <github@users.noreply.github.com>
2026-07-25 14:43:01 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 9e894375a9a0920bc1ff89a0066efbbae660902b

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784989606252,
+  "lastUpdate": 1784990581645,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -7963,6 +7963,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "Lines of code (app)",
             "value": 16329,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 10899,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1316,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 705,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 380.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 23,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 39.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "9e894375a9a0920bc1ff89a0066efbbae660902b",
+          "message": "Folder comparison: separate explicit modes for exact match vs. case-insensitive",
+          "timestamp": "2026-07-25T17:38:09+03:00",
+          "tree_id": "903576a6a2187de9488394d6eb44b50f8a3937e9",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/9e894375a9a0920bc1ff89a0066efbbae660902b"
+        },
+        "date": 1784990578138,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 16334,
             "unit": "LOC"
           },
           {
