067879320edac0e8fbc07b94d882fa75ee9ac1b8
github-action-benchmark <github@users.noreply.github.com>
2026-07-28 23:50:51 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 2e92722a5eb7a9912ad0e898f85cf75ad0783d2e

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785281436009,
+  "lastUpdate": 1785282651354,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -10738,6 +10738,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .text",
             "value": 716.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 381.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 22.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 40,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "2e92722a5eb7a9912ad0e898f85cf75ad0783d2e",
+          "message": "Cancellation/pause during directory enumeration",
+          "timestamp": "2026-07-29T02:45:32+03:00",
+          "tree_id": "9df11d520d4def90af9834d58d0b1610207543ab",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/2e92722a5eb7a9912ad0e898f85cf75ad0783d2e"
+        },
+        "date": 1785282649193,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 16870,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 10897,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1329,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 717,
             "unit": "KB"
           },
           {
