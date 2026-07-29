3d6864153c1c29088b56adcad9569fc2f33eb5fb
github-action-benchmark <github@users.noreply.github.com>
2026-07-29 00:02:35 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 9989345881413bd82eeaec6502f76e6533e4904d

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785282651354,
+  "lastUpdate": 1785283355685,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -10792,6 +10792,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "Lines of code (app)",
             "value": 16870,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 10897,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1329,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 717,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 381.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 22.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 40,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "9989345881413bd82eeaec6502f76e6533e4904d",
+          "message": "Consume delete writability authorization and avoid duplicate leaf move renames",
+          "timestamp": "2026-07-29T02:56:35+03:00",
+          "tree_id": "041ca332570ad1389fa1f0c2be5507cc2bda53a3",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/9989345881413bd82eeaec6502f76e6533e4904d"
+        },
+        "date": 1785283352335,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 16871,
             "unit": "LOC"
           },
           {
