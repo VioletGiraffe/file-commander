3ab91137ebdfe70739bd9a8f7ce2505e1e98a0e5
github-action-benchmark <github@users.noreply.github.com>
2026-08-01 19:07:14 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for c1af3f584624af94ee8436c5f048c0958833869e

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785609769961,
+  "lastUpdate": 1785611234106,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -12167,6 +12167,75 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/VioletGiraffe/file-commander/commit/56a99e281403e23209785984712149a63cb68e9d"
         },
         "date": 1785609767346,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 16994,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 11291,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1274,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 699,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 383,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 22.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 34,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "c1af3f584624af94ee8436c5f048c0958833869e",
+          "message": "CI: GCC 14 on Ubuntu",
+          "timestamp": "2026-08-01T22:01:53+03:00",
+          "tree_id": "d107fa8fd59bb2038d9296c6b7296360253cd0ac",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/c1af3f584624af94ee8436c5f048c0958833869e"
+        },
+        "date": 1785611231496,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
