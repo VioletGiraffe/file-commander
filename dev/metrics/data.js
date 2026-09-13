0db13df351e7989b451760aa7a5b57ad322f2067
github-action-benchmark <github@users.noreply.github.com>
2026-09-13 17:13:42 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 921b8fec83e3ef9ce521c7a72b633261638f7e20

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789318219921,
+  "lastUpdate": 1789319622483,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -18219,6 +18219,85 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .rdata",
             "value": 414.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 38.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 25.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "921b8fec83e3ef9ce521c7a72b633261638f7e20",
+          "message": "Text viewer: use CFindBar; CFindDialog removed",
+          "timestamp": "2026-09-13T20:08:23+03:00",
+          "tree_id": "2dbea7e890cd8fcfd5439274da27c397b8969db0",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/921b8fec83e3ef9ce521c7a72b633261638f7e20"
+        },
+        "date": 1789319618243,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 17253,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 16919,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1391.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 203,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2078,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 777,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 414,
             "unit": "KB"
           },
           {
