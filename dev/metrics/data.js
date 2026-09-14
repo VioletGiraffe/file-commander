744399a4be80dc5d83639b38724d2f65c88bbd5b
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 11:34:14 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for e3c8b809785c2b8fdce87a68218469634cb18a13

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789384354284,
+  "lastUpdate": 1789385653872,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -18974,6 +18974,85 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/VioletGiraffe/file-commander/commit/a6c449ad5d513f76c09e4138ec91eeb668deb156"
         },
         "date": 1789384350209,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 17258,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 17440,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1391.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 203,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2105,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 777,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 414,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 38.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 25.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "e3c8b809785c2b8fdce87a68218469634cb18a13",
+          "message": "Compiler warning flags rework",
+          "timestamp": "2026-09-14T14:28:03+03:00",
+          "tree_id": "a4a8aad6b802cd5fb2cc61c2a00f3bc32c9d0f29",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/e3c8b809785c2b8fdce87a68218469634cb18a13"
+        },
+        "date": 1789385651066,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
