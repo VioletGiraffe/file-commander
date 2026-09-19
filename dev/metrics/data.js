27044993144169c8aaf72f767630b8bcf9013f20
github-action-benchmark <github@users.noreply.github.com>
2026-09-19 12:08:01 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for f59ce31f89f1e78dbfd5bc09bd4faf52827c22da

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789819368873,
+  "lastUpdate": 1789819681228,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -21552,6 +21552,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/VioletGiraffe/file-commander/commit/4bcc8a4535ec606956268b7562b7c8f2ddf944ef"
         },
         "date": 1789819364835,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19101,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18077,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1440,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 203,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2126.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 812.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 425,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 40.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 25.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "f59ce31f89f1e78dbfd5bc09bd4faf52827c22da",
+          "message": "Windows: normal consoles also get the change-folder command; the working directory alone is unreliable",
+          "timestamp": "2026-09-19T15:00:59+03:00",
+          "tree_id": "835a7d5a056bd0b5ed07c86c7d37a0d22b2b35a8",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/f59ce31f89f1e78dbfd5bc09bd4faf52827c22da"
+        },
+        "date": 1789819677537,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
