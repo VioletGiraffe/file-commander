250f9f3df4ba2deeadcd71cbfdba7d49b1561d04
github-action-benchmark <github@users.noreply.github.com>
2026-09-19 10:58:00 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 1186cc06c7e5bd0f0ea317b58bdcb06db8e00cbc

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789780974160,
+  "lastUpdate": 1789815480208,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -21394,6 +21394,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "Lines of code (libs)",
             "value": 18075,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1439,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 203,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2126.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 812,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 424.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 40.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 25.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "1186cc06c7e5bd0f0ea317b58bdcb06db8e00cbc",
+          "message": "New splitShellWords() function, used by OsShell::runExecutable to call the program with the correct arguments list",
+          "timestamp": "2026-09-19T13:51:05+03:00",
+          "tree_id": "ab2387c7738118d248615941fc9c186915c691a8",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/1186cc06c7e5bd0f0ea317b58bdcb06db8e00cbc"
+        },
+        "date": 1789815477303,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19089,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18077,
             "unit": "LOC"
           },
           {
