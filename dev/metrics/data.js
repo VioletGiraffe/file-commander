ec266a60810f226d413bc5e5072e7f5897b75368
github-action-benchmark <github@users.noreply.github.com>
2026-09-20 15:29:04 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 4b6e4c19f884edff57b602681c952ed7ad52cd76

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789917228368,
+  "lastUpdate": 1789918144379,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -22056,6 +22056,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/VioletGiraffe/file-commander/commit/4d83dd034605a2d9758db3d21ec43d6a227a97f4"
         },
         "date": 1789917226109,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19085,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18077,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1420.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 199,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2122.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 798,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 420.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 39.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 26,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "4b6e4c19f884edff57b602681c952ed7ad52cd76",
+          "message": "CI: fixing macOS rpath",
+          "timestamp": "2026-09-20T18:22:10+03:00",
+          "tree_id": "b9232675e68c7ff3dfd05c34f2ec440118bcd7a2",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/4b6e4c19f884edff57b602681c952ed7ad52cd76"
+        },
+        "date": 1789918140267,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
