92952f106a41a7f1b32058d42373fbcb8688ace3
github-action-benchmark <github@users.noreply.github.com>
2026-09-22 20:32:15 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 5ac6f5c68d11fa165e269fc395f3fb3bf6e179a7

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790108022551,
+  "lastUpdate": 1790109135046,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -23153,6 +23153,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "Lines of code (app)",
             "value": 19330,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18336,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1424.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 240,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2122.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 801,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 421.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 39.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 26,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "5ac6f5c68d11fa165e269fc395f3fb3bf6e179a7",
+          "message": "Fixed: CFileSystemObject::setPath() didn't reset all fields and could keep stale values cached",
+          "timestamp": "2026-09-22T23:26:32+03:00",
+          "tree_id": "ac06b954b07d2b47e0e3c8bbea865310c72cfed9",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/5ac6f5c68d11fa165e269fc395f3fb3bf6e179a7"
+        },
+        "date": 1790109132151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19323,
             "unit": "LOC"
           },
           {
