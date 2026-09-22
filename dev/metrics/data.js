d4f14766e26f06fbe6199e8431fde40587e493a7
github-action-benchmark <github@users.noreply.github.com>
2026-09-22 23:45:40 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 619fe3540d59149ac2eddb12ab5c274beb47f48d

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790109139559,
+  "lastUpdate": 1790120740223,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -23371,6 +23371,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .data",
             "value": 26,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "619fe3540d59149ac2eddb12ab5c274beb47f48d",
+          "message": "CFileListSortFilterProxyModel deleted - merged into CFileListModel; the model now owns the row data; + tests",
+          "timestamp": "2026-09-23T02:38:16+03:00",
+          "tree_id": "ce94579d33c7eed50d6cfd5345eb43d6c340b6ec",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/619fe3540d59149ac2eddb12ab5c274beb47f48d"
+        },
+        "date": 1790120736905,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19393,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18339,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1424.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 240,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2123,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 804.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 417.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 39.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 26.5,
             "unit": "KB"
           },
           {
