0cf446664c00e6fad321270c91664a84f6c49259
github-action-benchmark <github@users.noreply.github.com>
2026-09-23 13:59:12 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 06fe0adb92e0231737180c905b097679b078666a

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790167149089,
+  "lastUpdate": 1790171952071,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -24122,6 +24122,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .pdata",
             "value": 40.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 26.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 10.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "06fe0adb92e0231737180c905b097679b078666a",
+          "message": "CFileSystemObject moved to thin_io (from QFileInfo)",
+          "timestamp": "2026-09-23T16:52:24+03:00",
+          "tree_id": "758ec4572bd271b66101a80ea028759de76849b3",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/06fe0adb92e0231737180c905b097679b078666a"
+        },
+        "date": 1790171947942,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19652,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18664,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1449.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 240,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2134,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 826,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 419.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 41,
             "unit": "KB"
           },
           {
