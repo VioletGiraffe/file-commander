966c8b878ab352c6a8965dcd1b3937fa1ed80846
github-action-benchmark <github@users.noreply.github.com>
2026-09-23 18:17:10 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 84b2d9bc79f1ab49140d6f2b872ea0d97355cf35

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790186987823,
+  "lastUpdate": 1790187430579,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -24492,6 +24492,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/VioletGiraffe/file-commander/commit/f9f44da5b1700239ea4f69acedab516906bde2de"
         },
         "date": 1790186983822,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19629,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18727,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1337.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 240,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2134,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 749.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 397.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 36.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 18.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "84b2d9bc79f1ab49140d6f2b872ea0d97355cf35",
+          "message": "CI: Ubuntu 22.04 ARM build added",
+          "timestamp": "2026-09-23T21:10:09+03:00",
+          "tree_id": "d449ee181a9efe6ba26eedc5bfa531aeaaa5254a",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/84b2d9bc79f1ab49140d6f2b872ea0d97355cf35"
+        },
+        "date": 1790187427572,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
