570136bf96ef2f4e4faab200001e8cf9f716621b
github-action-benchmark <github@users.noreply.github.com>
2026-09-25 14:14:09 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for c8d27a54cbb3147e00b9719692aa99c21fe4bdbb

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790189366991,
+  "lastUpdate": 1790345649474,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -24764,6 +24764,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "plugin_imageviewer.dll size",
             "value": 240,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2134,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 750.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 398,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 36.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 18.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "c8d27a54cbb3147e00b9719692aa99c21fe4bdbb",
+          "message": "CI: Qt 6.11.2 (6.11.3 fails to install on Windows)",
+          "timestamp": "2026-09-25T17:07:29+03:00",
+          "tree_id": "c6b61a9981b00b2f67b18859f52ceb44a1d37011",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/c8d27a54cbb3147e00b9719692aa99c21fe4bdbb"
+        },
+        "date": 1790345646489,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19629,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18970,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1339,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 246,
             "unit": "KB"
           },
           {
