c7894d4046ea9817e2b2872742aa0d6bec80b043
github-action-benchmark <github@users.noreply.github.com>
2026-09-26 14:20:09 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for 88dd2bea7bf391c61c8dfd23d0b710934858a549

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790431687168,
+  "lastUpdate": 1790432409475,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -25451,6 +25451,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .text",
             "value": 754,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 399.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 37,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 18.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "88dd2bea7bf391c61c8dfd23d0b710934858a549",
+          "message": "CIconProvider API fixed to accept CFileSystemObject and work with QStringView - no longer allocates full strings; also no longer re-caclulates the hash it already had",
+          "timestamp": "2026-09-26T17:13:57+03:00",
+          "tree_id": "d679e6fbf0ec9ccd4e82f3f47b5f8de3bc627023",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/88dd2bea7bf391c61c8dfd23d0b710934858a549"
+        },
+        "date": 1790432404482,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19657,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18995,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1345,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 234.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2134,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 754.5,
             "unit": "KB"
           },
           {
