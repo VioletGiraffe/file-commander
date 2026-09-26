976cfbacb431cde7353d0cefb30e5be90e0a551a
github-action-benchmark <github@users.noreply.github.com>
2026-09-26 13:01:58 +0000

    add File Commander metrics (customSmallerIsBetter) benchmark result for fb093e7c758062e9833f97a1dc5d1388305d5c34

--- dev/metrics/data.js
+++ dev/metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790424140567,
+  "lastUpdate": 1790427718149,
   "repoUrl": "https://github.com/VioletGiraffe/file-commander",
   "entries": {
     "File Commander metrics": [
@@ -25199,6 +25199,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "Section .text",
             "value": 754.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rdata",
+            "value": 399.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .rsrc",
+            "value": 125,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .pdata",
+            "value": 37,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .data",
+            "value": 18.5,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .reloc",
+            "value": 9.5,
+            "unit": "KB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "committer": {
+            "email": "VioletGiraffe@users.noreply.github.com",
+            "name": "Violet Giraffe",
+            "username": "VioletGiraffe"
+          },
+          "distinct": true,
+          "id": "fb093e7c758062e9833f97a1dc5d1388305d5c34",
+          "message": "NaturalSort: manually comparing empty-string keys because macOS does it wrong (in reverse, sorting empty after non-empty)",
+          "timestamp": "2026-09-26T15:54:41+03:00",
+          "tree_id": "38a782310bf1ef39e92dfcd103a0f478f0156fc7",
+          "url": "https://github.com/VioletGiraffe/file-commander/commit/fb093e7c758062e9833f97a1dc5d1388305d5c34"
+        },
+        "date": 1790427714545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Lines of code (app)",
+            "value": 19679,
+            "unit": "LOC"
+          },
+          {
+            "name": "Lines of code (libs)",
+            "value": 18995,
+            "unit": "LOC"
+          },
+          {
+            "name": "FileCommander.exe size",
+            "value": 1345.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_imageviewer.dll size",
+            "value": 234.5,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_textviewer.dll size",
+            "value": 2134,
+            "unit": "KB"
+          },
+          {
+            "name": "plugin_csvviewer.dll size",
+            "value": 361,
+            "unit": "KB"
+          },
+          {
+            "name": "Section .text",
+            "value": 755,
             "unit": "KB"
           },
           {
